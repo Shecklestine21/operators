4e4e9c6682012d9f01d15f30b78858a0d92b978a
Shecklestine21 <145370753+Shecklestine21@users.noreply.github.com>
2024-06-11 09:10:46 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// var random1 = Math.random() * 100
+// console.log(random1);
+
+
+// var random2 = Math.random() * 10;
+// random2 = Math.floor(random2);
+// console.log(random2);
+// //range
+// var random3 = Math.random() * 10 + (-5);
+// random3 = Math.floor(random3);
+// console.log(random3);
+
+
+// //example
+// var length = +prompt("length")
+// var width = +prompt("width")
+// var height = +prompt("height")
+
+// //check if less then 10
+// if (length > 10 || width > 10 || height > 10){
+//     alert("reject");
+// } else {
+//     alert("accept");
+// }
+
+//example grade application
+// var grade = +prompt("Grade");
+
+// if (grade >= 0 && grade <= 100){
+//     alert("Grade submitted")
+// } else {
+//     alert("invalid garde");
+// }
+
+var userNum = parseInt(prompt("Enter a single digit number."));
+// if (userNum >= 0 && userNum <= 9){
+// alert("thanks")
+// } else {
+// alert("thats not a single digit number")
+// }
+
+if (userNum == 0 || userNum == 1 || userNum == 3 || userNum == 4 || userNum == 5 || userNum == 6 || userNum == 7 || userNum == 8 || userNum == 9);
